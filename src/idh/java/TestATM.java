179c60152f190a4e6e08d6579f25be2a4c7e8091
Alex <alexander-vollbach@posteo.de>
2026-05-03 01:33:35 +0200

    testNeg mit Addition aktualisiert

--- src/idh/java/TestATM.java
+++ src/idh/java/TestATM.java
@@ -1,6 +1,7 @@
 package idh.java;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -26,7 +27,7 @@ public class TestATM {
 		assertTrue(
 				atm.convertToBills(95)[0] == 0 &&
 				atm.convertToBills(95)[1] == 0 &&				
-				atm.convertToBills(95)[2] == 0 &&
+				atm.convertToBills(95)[2] == 0 &&	
 				atm.convertToBills(95)[3] == 1 &&
 				atm.convertToBills(95)[4] == 2 &&
 				atm.convertToBills(95)[5] == 0 &&
@@ -47,30 +48,30 @@ public class TestATM {
 	@Test 
 	public void testNegConvertToBills() {
 		try {
-		assertTrue(
-				atm.convertToBills(-5)[0] == 0 &&
-				atm.convertToBills(-5)[1] == 0 &&				
-				atm.convertToBills(-5)[2] == 0 &&
-				atm.convertToBills(-5)[3] == 0 &&
-				atm.convertToBills(-5)[4] == 0 &&
-				atm.convertToBills(-5)[5] == 0 &&
-				atm.convertToBills(-5)[6] == 0);
-		assertTrue(
-				atm.convertToBills(-100)[0] == 0 &&
-				atm.convertToBills(-100)[1] == 0 &&				
-				atm.convertToBills(-100)[2] == 0 &&
-				atm.convertToBills(-100)[3] == 0 &&
-				atm.convertToBills(-100)[4] == 0 &&
-				atm.convertToBills(-100)[5] == 0 &&
-				atm.convertToBills(-100)[6] == 0);
-		assertTrue(
-				atm.convertToBills(-27)[0] == 0 &&
-				atm.convertToBills(-27)[1] == 0 &&				
-				atm.convertToBills(-27)[2] == 0 &&
-				atm.convertToBills(-27)[3] == 0 &&
-				atm.convertToBills(-27)[4] == 0 &&
-				atm.convertToBills(-27)[5] == 0 &&
-				atm.convertToBills(-27)[6] == 0);
+		assertEquals(0, 
+				atm.convertToBills(-5)[0] +
+				atm.convertToBills(-5)[1] +				
+				atm.convertToBills(-5)[2] +
+				atm.convertToBills(-5)[3] +
+				atm.convertToBills(-5)[4] +
+				atm.convertToBills(-5)[5] +
+				atm.convertToBills(-5)[6]);
+		assertEquals(0,
+				atm.convertToBills(-100)[0] +
+				atm.convertToBills(-100)[1] +				
+				atm.convertToBills(-100)[2] +
+				atm.convertToBills(-100)[3] +
+				atm.convertToBills(-100)[4] +
+				atm.convertToBills(-100)[5] +
+				atm.convertToBills(-100)[6]);
+		assertEquals(0,
+				atm.convertToBills(-27)[0] +
+				atm.convertToBills(-27)[1] +				
+				atm.convertToBills(-27)[2] +
+				atm.convertToBills(-27)[3] +
+				atm.convertToBills(-27)[4] +
+				atm.convertToBills(-27)[5] +
+				atm.convertToBills(-27)[6]);
 		}catch (IllegalInputException e) {
 			fail();
 			}
