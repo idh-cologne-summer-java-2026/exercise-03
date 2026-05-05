5400ce442bd90199805de9853e6434f9e88c8473
bitbunny161 <reckert.hanna@gmail.com>
2026-05-05 16:17:29 +0200

    Hallo

--- src/idh/java/OutOfRangeException.java
+++ src/idh/java/OutOfRangeException.java
@@ -2,6 +2,9 @@ package idh.java;
 
 public class OutOfRangeException extends Exception {
 
-	private static final long serialVersionUID = 1L;
-
+private static final long serialVersionUID = 1L;
+	
+	public OutOfRangeException (String S) {
+		System.out.println(S); 
+	}
 }
