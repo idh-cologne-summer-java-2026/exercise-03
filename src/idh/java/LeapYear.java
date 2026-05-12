c391262e2e1384834dd0acf53e12187264730bc7
yonca <yonca@Yonca.fritz.box>
2026-05-12 19:28:42 +0200

    Hausaufgabe fertig

--- src/idh/java/LeapYear.java
+++ src/idh/java/LeapYear.java
@@ -2,6 +2,24 @@ package idh.java;
 
 public class LeapYear {
 	public boolean isLeapYear(int year) throws OutOfRangeException {
+		
+		// vor dem gregorianischen Kalender nicht definiert
+		if(year <1582) {
+			throw new OutOfRangeException();
+		}
+		
+		if(year %4 ==0) {
+			return true;
+		}
+		
+		
+		if(year %400==0) {
+			return true;
+		}
+		if(year %100==0) {
+			return false;
+		}
+		
 		return false;
 	}
 }
