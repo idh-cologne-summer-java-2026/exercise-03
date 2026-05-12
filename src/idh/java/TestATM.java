2c2bdd731a130697a3bf941349f3d3df8bb9304b
Lavi <Lavi@172.29.177.143>
2026-05-12 17:40:33 +0200

    ich bin fertig!

--- src/idh/java/TestATM.java
+++ src/idh/java/TestATM.java
@@ -1,5 +1,24 @@
 package idh.java;
 
-public class TestATM {
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.api.Test;
 
-}
+public class TestATM {
+	
+	private final ATM atm = new ATM();
+
+	@Test
+	public void testConvertToBills_MinimumAmount() throws ATM.IllegalInputException {
+		int[] expected = new int[] {0, 0, 0, 0, 0, 0, 0, 1};
+		int[] actual = atm.convertToBills(5);
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	public void testConvertToBills_LargeAmount() throws ATM.IllegalInputException {
+		int[] expected = new int[] {1, 0, 0, 0, 0, 0, 0, 0};
+		int[] actual = atm.convertToBills(500);
+		assertArrayEquals(expected, actual);
+	}
+} 
--- src/idh/java/TestLeapYear.java
+++ src/idh/java/TestLeapYear.java
@@ -43,5 +43,5 @@ public class TestLeapYear {
 		assertThrows(OutOfRangeException.class, () -> leapYearObject.isLeapYear(-2000));
 
 	}
-
+	
 }
