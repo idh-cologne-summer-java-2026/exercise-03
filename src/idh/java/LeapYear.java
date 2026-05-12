cdb7adb7e383cfd8448458ad1ac308decff06047
ToonLinkHD <carlo.ital@web.de>
2026-05-13 00:18:30 +0200

    Update LeapYear.java

--- src/idh/java/LeapYear.java
+++ src/idh/java/LeapYear.java
@@ -2,8 +2,18 @@ package idh.java;
 
 public class LeapYear {
 	public boolean isLeapYear(int year) throws OutOfRangeException {
-		return false;
+		 if (year < 1582) {
+	            throw new OutOfRangeException();
+	        }
+
+	        if (year % 400 == 0) {
+	            return true;
+	        }
+
+	        if (year % 100 == 0) {
+	            return false;
+	        }
+
+	        return year % 4 == 0;
 	}
 }
-
-
